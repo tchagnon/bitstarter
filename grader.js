0551b8a59d236a6dd32afdba586e7189e8a5fcc6
Tim Chagnon <tim.chagnon@gmail.com>
2013-07-10 22:43:20 -0700

    Add additional checks and --url option to grader.

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -36,16 +37,26 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+var htmlFileLoader = function(file) {
+  var data = fs.readFileSync(file);
+  return function(callback) {
+    callback(data);
+  }
+};
+
+var urlFileLoader = function(url) {
+  return function(callback) {
+    console.log('fetching ' + url);
+    rest.get(url).on('complete', callback);
+  }
 };
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtml = function(htmlData, checksfile) {
+    $ = cheerio.load(htmlData);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -64,11 +75,27 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists))
+        .option('-u, --url <url>', 'URL to index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    if (!program.file && !program.url) {
+      console.error('One of --file or --url is required.');
+      return;
+    }
+
+    var loader;
+    if (program.file) {
+      loader = htmlFileLoader(program.file);
+    } else {
+      loader = urlFileLoader(program.url);
+    }
+
+    loader(function(htmlContent) {
+      var checkJson = checkHtml(htmlContent, program.checks);
+      var outJson = JSON.stringify(checkJson, null, 4);
+      console.log(outJson);
+    });
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
